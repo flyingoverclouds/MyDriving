772c47862b3d8acb960f7527f35762e20a6a9ac7
Nicolas CLERC <flyingoverclouds@users.noreply.github.com>
2021-12-02 10:57:13 +0100

    Create api-status-check.sh
    add a wait correct status for API

---
+++ apis/user-java/api-status-check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+declare endpoint="https://openhackiaw3kcz1userjava.azurewebsites.net/api/healthcheck/user-java" #should be parameterized
+declare -i maxretries=50
+declare -i retryinterval=2
+declare checkedstatus='json_status="healthy"'
+
+
+while [[ true ]]; do
+#   echo "$maxretries Checking $endpoint "
+   result=`curl --max-time 5 -s $endpoint |\
+     sed -re 's@(\[|\]|\{|\})@@g' -e 's/,/\n/g' |\
+     sed -re 's@"(\w+)":\s*"?([^"]*)"?@json_\1="\2"@g' | \
+     grep $checkedstatus |\
+     wc -l`
+#   echo "result: [$result]"
+   if [[ $result == "0" ]]; then
+      echo "unhealthy status"
+   else
+      echo "healthy status"
+      exit 0
+   fi
+   if [[ $maxretries == 0 ]]; then
+      echo " Too many retry : healthy check failed"
+      exit 1;
+   fi
+
+   sleep $retryinterval
+   let maxretries--
+done
